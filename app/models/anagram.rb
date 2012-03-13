a6c76e8cd333b283bf48ea6bb40b011705e416fb
Przemek Hocke <phocke@gmail.com>
2012-03-13 21:03:42 +0000

    forgot about puts

--- app/models/anagram.rb
+++ app/models/anagram.rb
@@ -3,7 +3,6 @@ class Anagram < ActiveRecord::Base
   def self.handle_upload(file)
     anagrams_file = file
     
-    puts anagrams_file.inspect
     begin
       File.open(Rails.root.join('public', 'uploads', anagrams_file.original_filename), 'w') do |file|
         file.write(anagrams_file.read)
