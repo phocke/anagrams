26495bed4cc8637411289db052d0eeb43e5146ef
Przemek Hocke <phocke@gmail.com>
2012-03-13 20:29:47 +0000

    cleanup the anagrams controller

--- app/controllers/anagrams_controller.rb
+++ app/controllers/anagrams_controller.rb
@@ -1,83 +1,31 @@
 class AnagramsController < ApplicationController
-  # GET /anagrams
-  # GET /anagrams.json
+
   def index
     @anagrams = Anagram.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @anagrams }
-    end
   end
 
-  # GET /anagrams/1
-  # GET /anagrams/1.json
   def show
     @anagram = Anagram.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @anagram }
-    end
   end
 
-  # GET /anagrams/new
-  # GET /anagrams/new.json
   def new
-    @anagram = Anagram.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @anagram }
-    end
+    @anagrams = Anagram.new
   end
 
-  # GET /anagrams/1/edit
-  def edit
-    @anagram = Anagram.find(params[:id])
-  end
-
-  # POST /anagrams
-  # POST /anagrams.json
   def create
-    @anagram = Anagram.new(params[:anagram])
+    @anagrams = handle_file(params[:anagram])
 
-    respond_to do |format|
-      if @anagram.save
-        format.html { redirect_to @anagram, notice: 'Anagram was successfully created.' }
-        format.json { render json: @anagram, status: :created, location: @anagram }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @anagram.errors, status: :unprocessable_entity }
-      end
+    if @anagrams.save
+      flash[:notice] = "Anagrams created"
+      render action: "index"
+    else
+      render action: "new" 
     end
   end
 
-  # PUT /anagrams/1
-  # PUT /anagrams/1.json
-  def update
-    @anagram = Anagram.find(params[:id])
+  private
 
-    respond_to do |format|
-      if @anagram.update_attributes(params[:anagram])
-        format.html { redirect_to @anagram, notice: 'Anagram was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @anagram.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  def handle_file(params)
 
-  # DELETE /anagrams/1
-  # DELETE /anagrams/1.json
-  def destroy
-    @anagram = Anagram.find(params[:id])
-    @anagram.destroy
-
-    respond_to do |format|
-      format.html { redirect_to anagrams_url }
-      format.json { head :no_content }
-    end
   end
 end
