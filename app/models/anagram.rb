0d81c7176ef1e6239599ebb27609da923a3473a8
Przemek Hocke <phocke@gmail.com>
2012-03-14 11:23:15 +0000

    create multiple, and handle uploads better

--- app/models/anagram.rb
+++ app/models/anagram.rb
@@ -1,16 +1,36 @@
 class Anagram < ActiveRecord::Base
 
-  def self.handle_upload(file)
-    anagrams_file = file
-    
-    begin
-      File.open(Rails.root.join('public', 'uploads', anagrams_file.original_filename), 'w') do |file|
-        file.write(anagrams_file.read)
+  validates :word, presence: true, uniqueness: true
+
+  class << self
+    def handle_upload(file)
+      anagrams_file = file
+      
+      begin
+        uploaded_file_name = Rails.root.join('public', 'uploads', anagrams_file.original_filename)
+        
+        File.open(uploaded_file_name, 'w') do |file|
+          file.write(anagrams_file.read)
+        end
+
+        create_multiple(uploaded_file_name)
+
+        return true
+      rescue
+        return false
       end
-      return true
-    rescue
-      return false
     end
+
+    def create_multiple(file_name)
+      File.open(file_name, 'r') do |file|
+        while (line = file.gets)
+          word = line.chomp
+          next unless Anagram.create word: word, sorted_word: word.sorted          
+        end
+      end      
+    end
+
   end
+
 
 end
